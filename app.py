fce9a44bab440f07298e151616702e51e7866e6d
Luis Guerra <45185410+Luis0293@users.noreply.github.com>
2019-05-15 12:23:01 +0100

    Add files via upload
    Login

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/p1')
-def p1():
-    return render_template('p1.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/p2')
 def p2():
